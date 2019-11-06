ae5b6fd49bcba8d266b0a63760d266fcf3cbe9db
Priyanshu-ai <57174397+Priyanshu-ai@users.noreply.github.com>
2019-11-06 14:13:11 +0530

    Update Open file.py

--- Open file.py
+++ Open file.py
@@ -2,18 +2,23 @@ import random
 print("Here, I am your Professional Friendly Bot... You can call me PROGPOT")
 list0 = ["Heya!!","Hello","Hi"]
 dict1 = {"how are you?","what's up","whatsup","how are you doing","how are you","how are you???","how are you??"}
-list1 = ["I am fine and you","Fine","Okay","I am fine"]
 text = input(":-->>")
+list1 = ["i am fine.","Fine","Doing Good"]
 if "hi" in text.lower() or "hello" in text.lower():
     print(random.choice(list0))
+    text0 = input(":-->>")
+    if text0.lower() in dict1:
+        print(random.choice(list1))
+        print("\n\n-->>>Here,I am PROGPOT and I am glad that you came here... so What you want to do ?<<<----")
+    else:
+        print("Leave it, First tell me How are you ")
+        input(":-->>")
+        print("\n\n-->>>Here,I am PROGPOT and I am glad that you came here... so What you want to do ?<<<----")
 else:
-    print("Sorry, Go and learn some Manners")
-text0 = input(":-->>")
-if text0.lower() in dict1:
-    print(random.choice(list1))
-else:
-    print("Leave it, First tell me How are you ")
-    input(":-->>")
-print("\n\n-->>>Here,I am PROGPOT and I am glad that you came here... so What you want to do ?<<<----")
+    print("\n\n-->>>Here,I am PROGPOT and I am glad that you came here... so What you want to do ?<<<----")
+    ask0 = input("What You want to know?")
+    print(ask0)
+
+
 
 
